c0e8576a313f7b80692822ad21cf726cda9a7acd
vicentlozano <lozanodroid@gmail.com>
2025-01-29 20:43:33 +0100

    correjit class test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,8 +2,9 @@ import com.example.Application;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class AppTest{
-    @testpublic void testApp() {
+public class AppTest {
+    @Test  // <-- Corregit, ara és una anotació vàlida
+    public void testApp() {
         Application myApp = new Application();
         String result = myApp.getStatus();
         assertEquals("OK", result);
